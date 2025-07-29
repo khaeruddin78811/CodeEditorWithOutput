7c57f6281c8d94be896b9cfaa9a80d25f4bc9dcd
khaeruddin78811 <65596410+khaeruddin78811@users.noreply.github.com>
2025-07-29 15:12:06 +0330

    server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const executeRoutes = require("./routes/execute");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/execute", executeRoutes);
+
+app.listen(3000, () => console.log("Server running on port 3000"));
