811e659ccb25f5e66814b33126a9161bbd147077
khaeruddin78811 <65596410+khaeruddin78811@users.noreply.github.com>
2025-07-29 15:04:18 +0330

    CodeEditor.js

--- client/src/components/CodeEditor.js
+++ client/src/components/CodeEditor.js
@@ -1,1 +1,24 @@
 
+import Editor from "@monaco-editor/react";
+
+function CodeEditor({ code, setCode, language }) {
+  return (
+    <Editor
+      height="60vh"
+      language={language}
+      value={code[language]}
+      onChange={(value) => setCode({ ...code, [language]: value })}
+      theme="vs-dark"
+      options={{
+        minimap: { enabled: false },
+        fontSize: 14,
+        autoIndent: true,
+        formatOnPaste: true,
+        suggestOnTriggerCharacters: true,
+        wordWrap: "on"
+      }}
+    />
+  );
+}
+
+export default CodeEditor;
